0eb3c64c07cc033c624e2b8fad47248739ec1114
Sanket Vagadiya <scvagadiya@gmail.com>
2024-03-05 17:44:18 -0500

    updated language enum a

--- src/enums/languages.enum.ts
+++ src/enums/languages.enum.ts
@@ -1,39 +1,8 @@
-// export enum Languages {
-//     Sanskrit = 1,
-//     Gujarati,
-//     English,
-//     Hindi,
-//     Marathi,
-//     Telugu,
-//     Malyalam,
-//     Kannad
-// }
- 
-
 export enum Languages {
-    Sanskrit = 11,
-    Hindi=12,
-    added=20,
-    NewLanguage=21,
-    malyali=22,
+    Sanskrit=21,
+    Hindi=22,
     Gujarati=23,
-    English,
-    Hindi1,
-    Hindi2,
-    Hindi3,
-    Hindi4,
-    Marathi,
-    
-    Telugu,
-    Malyalam,
-    Kannad,
-    Kannad1,
-    Kannad2,
-    Kannad3,
-    Kannad4,
-    Kannad5,
-    Kannad6,
-    Kannad7,
-     
-
+    English=24
 }
+ 
+ 
