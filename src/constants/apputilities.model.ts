a36e064a15dcad11cb416dac1292c793f3ae447b
Sanket Vagadiya <scvagadiya@gmail.com>
2024-03-22 22:54:10 -0400

    updated base url

--- src/constants/apputilities.model.ts
+++ src/constants/apputilities.model.ts
@@ -2,7 +2,7 @@ import { Languages } from "../enums/languages.enum";
 
 export class AppUtilites {
 
-    public static API_BASE_URL = "http://18.222.183.218:7000/";
+    public static API_BASE_URL = "https://omnamahshivay-api-4f107e2d42b2.herokuapp.com/";
     public static getLanguageName(languageEnum: Languages = Languages.Hindi): string {
         return Languages[languageEnum].toString();
     }
