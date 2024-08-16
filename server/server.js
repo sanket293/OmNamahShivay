00aabc7a27bece380353e0fd08eff8732b6ffb12
scvagadiya <scvagadiya@gmail.com>
2024-08-15 21:28:24 -0400

    updated port to 5000

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const xmlbuilder = require('xmlbuilder');
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 const routes = [
     '/',
